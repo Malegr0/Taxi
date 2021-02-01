ea49bef87e2495de7d6309278edade99782f77b8
Malegr0 <schoening.andreas@gmx.de>
2021-02-01 16:03:23 +0100

    added methods to taxi.h

--- Taxi/taxi.h
+++ Taxi/taxi.h
@@ -8,4 +8,13 @@ private:
 	float m_consumption;
 	float m_fare_per_kilometer;
 	float m_balance;
+
+public:
+	void init(float km, float max_tank_capacity, float consumption, float fare_per_kilometer);
+
+	void drive(float distance, bool guest);
+
+	void refuel(float gas_price);
+
+	void output();
 };
