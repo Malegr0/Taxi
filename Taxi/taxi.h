30f58f965f9eace951662098b2080dcee93b222e
Malegr0 <schoening.andreas@gmx.de>
2021-02-01 15:53:48 +0100

    added taxi.h and member variables

---
+++ Taxi/taxi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class taxi {
+private:
+	float m_km;
+	float m_current_tank_capacity;
+	float m_max_tank_capacity;
+	float m_consumption;
+	float m_fare_per_kilometer;
+	float m_balance;
+};
