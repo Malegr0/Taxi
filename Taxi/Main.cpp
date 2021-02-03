574210f13c75be7a16ea4696d44864312d13fd3e
Malegr0 <schoening.andreas@gmx.de>
2021-02-03 21:45:12 +0100

    case of taxi1 and taxi2 with their methods added

--- Taxi/Main.cpp
+++ Taxi/Main.cpp
@@ -8,7 +8,7 @@ int main() {
 	taxi1.init(0.0f, 55.0f, 0.0572f, 2.0f);
 	taxi2.init(25.0f, 80.0f, 0.07f, 2.5f);
 
-	taxi1.drive(100.0f, true);
+	/*taxi1.drive(100.0f, true);
 	taxi1.output();
 
 	taxi2.drive(10.0f, false);
@@ -18,7 +18,151 @@ int main() {
 	taxi1.output();
 
 	taxi2.refuel(1.23f);
-	taxi2.output();
+	taxi2.output();*/
+
+	bool running = true;
+	char input = ' ';
+	while (running) {
+		switch (input) {
+			case '1': {
+				bool running_taxi_1 = true;
+				char t1_input = ' ';
+				while (running_taxi_1) {
+					switch (t1_input) {
+					case '1':
+						float distance;
+						char c_guest;
+						bool b_guest;
+						std::cout << "Es soll eine Fahrt verbucht werden." << std::endl;
+						std::cout << "Welche Distanz soll die Fahrt haben?" << std::endl;
+						std::cin >> distance;
+						if (distance < 0) {
+							std::cout << "Es können keine negativen Kilometer gefahren werden." << std::endl;
+							break;
+						}
+						std::cout << "Wird ein Kunde befördert? ja -> y; nein ->n" << std::endl;
+						std::cin >> c_guest;
+						if (c_guest == 'y') {
+							b_guest = true;
+						}
+						else if (c_guest == 'n'){
+							b_guest = false;
+						}
+						else {
+							std::cout << "Dies war eine falsche Eingabe!" << std::endl;
+							break;
+						}
+						taxi1.drive(distance, b_guest);
+						break;
+					case '2':
+						std::cout << "Es soll getankt werden." << std::endl;
+						taxi1.refuel(1.23);
+						break;
+					case '3':
+						taxi1.output();
+						break;
+					case 'x':
+						running_taxi_1 = false;
+						break;
+					default:
+						std::cout << "\nTaxi 1 wurde ausgewählt. Folgende Optionen sind möglich:" << std::endl;
+						std::cout << "Fahrt verbuchen -> 1" << std::endl;
+						std::cout << "Tanken -> 2" << std::endl;
+						std::cout << "Ausgabe des Taxiinformationen -> 3" << std::endl;
+						std::cout << "Zurück zum Hauptmenü -> x" << std::endl;
+						break;
+					}
+					std::cin >> t1_input;
+				}
+				std::cout << "Es muss eine mögliche Option ausgewählt werden.\n" << "Diese Optionen sind folgende:" << std::endl;
+				std::cout << "Taxi 1 -> 1" << std::endl;
+				std::cout << "Taxi 2 -> 2" << std::endl;
+				std::cout << "Programm beenden -> x" << std::endl;
+				break;
+			}
+			case '2': {
+				bool running_taxi_2 = true;
+				char t2_input = ' ';
+				while (running_taxi_2) {
+					switch (t2_input) {
+					case '1':
+						float distance;
+						char c_guest;
+						bool b_guest;
+						std::cout << "\nEs soll eine Fahrt verbucht werden." << std::endl;
+						std::cout << "Welche Distanz soll die Fahrt haben?" << std::endl;
+						std::cin >> distance;
+						if (distance < 0) {
+							std::cout << "Es können keine negativen Kilometer gefahren werden." << std::endl;
+							break;
+						}
+						std::cout << "Wird ein Kunde befördert? ja -> y; nein ->n" << std::endl;
+						std::cin >> c_guest;
+						if (c_guest == 'y') {
+							b_guest = true;
+						}
+						else if (c_guest == 'n') {
+							b_guest = false;
+						}
+						else {
+							std::cout << "Dies war eine falsche Eingabe!" << std::endl;
+							break;
+						}
+						taxi2.drive(distance, b_guest);
+						std::cout << "\nTaxi 2 wurde ausgewählt. Folgende Optionen sind möglich:" << std::endl;
+						std::cout << "Fahrt verbuchen -> 1" << std::endl;
+						std::cout << "Tanken -> 2" << std::endl;
+						std::cout << "Ausgabe des Taxiinformationen -> 3" << std::endl;
+						std::cout << "Zurück zum Hauptmenü -> x" << std::endl;
+						break;
+					case '2':
+						std::cout << "\nEs soll getankt werden." << std::endl;
+						taxi2.refuel(1.23);
+						std::cout << "\nTaxi 2 wurde ausgewählt. Folgende Optionen sind möglich:" << std::endl;
+						std::cout << "Fahrt verbuchen -> 1" << std::endl;
+						std::cout << "Tanken -> 2" << std::endl;
+						std::cout << "Ausgabe des Taxiinformationen -> 3" << std::endl;
+						std::cout << "Zurück zum Hauptmenü -> x" << std::endl;
+						break;
+					case '3':
+						taxi2.output();
+						std::cout << "\nTaxi 2 wurde ausgewählt. Folgende Optionen sind möglich:" << std::endl;
+						std::cout << "Fahrt verbuchen -> 1" << std::endl;
+						std::cout << "Tanken -> 2" << std::endl;
+						std::cout << "Ausgabe des Taxiinformationen -> 3" << std::endl;
+						std::cout << "Zurück zum Hauptmenü -> x" << std::endl;
+						break;
+					case 'x':
+						running_taxi_2 = false;
+						break;
+					default:
+						std::cout << "\nTaxi 2 wurde ausgewählt. Folgende Optionen sind möglich:" << std::endl;
+						std::cout << "Fahrt verbuchen -> 1" << std::endl;
+						std::cout << "Tanken -> 2" << std::endl;
+						std::cout << "Ausgabe des Taxiinformationen -> 3" << std::endl;
+						std::cout << "Zurück zum Hauptmenü -> x" << std::endl;
+						break;
+					}
+					std::cin >> t2_input;
+				}
+				std::cout << "Es muss eine mögliche Option ausgewählt werden.\n" << "Diese Optionen sind folgende:" << std::endl;
+				std::cout << "Taxi 1 -> 1" << std::endl;
+				std::cout << "Taxi 2 -> 2" << std::endl;
+				std::cout << "Programm beenden -> x" << std::endl;
+				break;
+			}
+				break;
+			case 'x':
+				std::cout << "Das Programm wird beendet." << std::endl;
+				running = false;
+			default:
+				std::cout << "Es muss eine mögliche Option ausgewählt werden.\n" << "Diese Optionen sind folgende:" << std::endl;
+				std::cout << "Taxi 1 -> 1" << std::endl;
+				std::cout << "Taxi 2 -> 2" << std::endl;
+				std::cout << "Programm beenden -> x" << std::endl;
+		}
+		std::cin >> input ;
+	}
 
 	return 0;
 }
--- Taxi/Taxi.cpp
+++ Taxi/Taxi.cpp
@@ -7,22 +7,22 @@ void taxi::init(float km, float max_tank_capacity, float consumption, float fare
 	this->m_consumption = consumption;
 	this->m_fare_per_kilometer = fare_per_kilometer;
 
-	this->m_current_tank_capacity = this->m_max_tank_capacity;
+	this->m_current_tank_capacity = m_max_tank_capacity;
 	this->m_balance = 0.0f;
 }
 
 void taxi::drive(float distance, bool guest) {
 	if (distance>=0.0f) {
-		if (!(distance * this->m_consumption > this->m_current_tank_capacity)) {
-			this->m_km = this->m_km + distance;
-			this->m_current_tank_capacity = this->m_current_tank_capacity - distance * this->m_consumption;
+		if (!(distance * m_consumption > m_current_tank_capacity)) {
+			this->m_km = m_km + distance;
+			this->m_current_tank_capacity = m_current_tank_capacity - distance * m_consumption;
 			if (guest) {
-				this->m_balance = this->m_balance + distance * this->m_fare_per_kilometer;
+				this->m_balance = m_balance + distance * m_fare_per_kilometer;
 			}
 			std::cout << "Folgende Fahrt wurde durchgeführt." << std::endl;
 			std::cout << "Gefahrene Kilometer: " << distance << std::endl;
-			std::cout << "Genutzter Treibstoff: " << distance * this->m_consumption << std::endl;
-			std::cout << "Eingenommenes Geld: " << distance * this->m_fare_per_kilometer << std::endl << std::endl;
+			std::cout << "Genutzter Treibstoff: " << distance * m_consumption << std::endl;
+			std::cout << "Eingenommenes Geld: " << distance * m_fare_per_kilometer << std::endl << std::endl;
 		}
 		else {
 			std::cout << "Für die eingegebene Distanz reicht der momentane Tankinhalt nicht mehr aus. Die Fahrt wird nicht begonnen" << std::endl << std::endl;
@@ -34,22 +34,34 @@ void taxi::drive(float distance, bool guest) {
 }
 
 void taxi::refuel(float gas_price) {
-	float missing_fuel = this->m_max_tank_capacity - this->m_current_tank_capacity;
-	if (missing_fuel * gas_price < this->m_balance) {
-		this->m_current_tank_capacity = this->m_max_tank_capacity;
-		this->m_balance = this->m_balance - missing_fuel * gas_price;
+	float missing_fuel = m_max_tank_capacity - m_current_tank_capacity;
+	char c_input;
+	if (missing_fuel * gas_price < m_balance) {
+		this->m_current_tank_capacity = m_max_tank_capacity;
+		this->m_balance = m_balance - missing_fuel * gas_price;
 		std::cout << "Der Tank wurde aufgefühlt. Es wurden " << missing_fuel * gas_price << " berechnet." << std::endl << std::endl;
 	} else {
 		std::cout << "Der Geldstand von " << this->m_balance << " reicht nicht aus, um den Tank vollzutanken." << std::endl;
 		std::cout << "Soll der Tank mit dem vorhandenen Geld so weit wie möglich vollgetankt werden? Folgende Antworten sind möglich:" << std::endl;
 		std::cout << "ja -> y" << std::endl;
 		std::cout << "nein -> n" << std::endl;
-		//TODO if oder case abfrage einfügen
+		std::cin >> c_input;
+		if (c_input == 'y') {
+			float fuel = m_balance / gas_price;
+			this->m_current_tank_capacity = m_current_tank_capacity + fuel;
+			this->m_balance = 0.0f;
+		}
+		else if (c_input == 'n') {
+			std::cout << "Es wurde nicht aufgetankt." << std::endl;
+		}
+		else {
+			std::cout << "Dies war eine falsche Eingabe." << std::endl;
+		}
 	}
 }
 
 void taxi::output() {
-	std::cout << "Aktueller Kilometerstand: " << this->m_km << std::endl;
-	std::cout << "Aktueller Tankinhalt: " << this->m_current_tank_capacity << std::endl;
-	std::cout << "Aktuelle Geldbilanz: " << this->m_balance << std::endl << std::endl;
+	std::cout << "Aktueller Kilometerstand: " << m_km << std::endl;
+	std::cout << "Aktueller Tankinhalt: " << m_current_tank_capacity << std::endl;
+	std::cout << "Aktuelle Geldbilanz: " << m_balance << std::endl << std::endl;
 }
